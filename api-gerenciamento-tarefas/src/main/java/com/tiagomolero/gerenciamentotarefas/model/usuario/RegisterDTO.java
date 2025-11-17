ccc3b28c9fc1931a5af854a289adcee33cb563f4
Tiago Molero <tiagomolero@gmail.com>
2025-11-17 00:33:14 -0300

    Validação do formato do E-mail

--- api-gerenciamento-tarefas/src/main/java/com/tiagomolero/gerenciamentotarefas/model/usuario/RegisterDTO.java
+++ api-gerenciamento-tarefas/src/main/java/com/tiagomolero/gerenciamentotarefas/model/usuario/RegisterDTO.java
@@ -7,7 +7,7 @@ public record RegisterDTO(
         @Size(min = 3, message = "Nome deve ter no mínimo 3 caracteres")
         String nome,
 
-        @Email
+        @Email(message = "Email Inválido! Informe um email válido",regexp = ".+[@].+[\\\\.].+")
         String email,
 
         @Size(min = 8, message = "Senha deve ter no mínimo 8 caracteres")
