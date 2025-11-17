ec8d70c8548f9efe3635f25c85d75a7e2ad3f983
Tiago Molero <tiagomolero@gmail.com>
2025-11-17 00:34:18 -0300

    Validação do formato do E-mail Login

--- api-gerenciamento-tarefas/src/main/java/com/tiagomolero/gerenciamentotarefas/model/usuario/AuthenticationDTO.java
+++ api-gerenciamento-tarefas/src/main/java/com/tiagomolero/gerenciamentotarefas/model/usuario/AuthenticationDTO.java
@@ -1,8 +1,11 @@
 package com.tiagomolero.gerenciamentotarefas.model.usuario;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
 public record AuthenticationDTO(
+
+        @Email(message = "Email Inválido! Informe um email válido",regexp = ".+[@].+[\\\\.].+")
         String email,
 
         @Size(min = 8, message = "Senha deve ter no mínimo 8 caracteres")
