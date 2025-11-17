8c45e16c111c446383ee3c97a936d39060251320
Tiago Molero <tiagomolero@gmail.com>
2025-11-17 19:25:55 -0300

    Ajuste validação de e-mail e senha

--- api-gerenciamento-tarefas/src/main/java/com/tiagomolero/gerenciamentotarefas/model/usuario/RegisterDTO.java
+++ api-gerenciamento-tarefas/src/main/java/com/tiagomolero/gerenciamentotarefas/model/usuario/RegisterDTO.java
@@ -1,15 +1,17 @@
 package com.tiagomolero.gerenciamentotarefas.model.usuario;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RegisterDTO(
         @Size(min = 3, message = "Nome deve ter no mínimo 3 caracteres")
         String nome,
 
-        @Email(message = "Email Inválido! Informe um email válido",regexp = ".+[@].+[\\\\.].+")
+        @Email(message = "Formato de e-mail inválido", regexp = "^[\\w.%+-]+@[\\w.-]+\\.[A-Za-z]{2,}$")
         String email,
 
         @Size(min = 8, message = "Senha deve ter no mínimo 8 caracteres")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).+$", message = "A senha deve conter pelo menos 1 letra e 1 número")
         String senha
 ) {}
